b7e4b6c1006b91088b5f2d272a2b42f3a466be5c
Foysal Ahmmed Limon <afaysal410@gmail.com>
2026-05-06 17:42:54 +0600

    feat: add ITourType interface and tour model with schema for tour management

---
+++ src/app/modules/tour/tour.model.ts
@@ -0,0 +1,49 @@
+import { model, Schema } from "mongoose";
+
+import type { ITour, ITourType } from "./tour.interface.js";
+
+const tourTypeSchema = new Schema<ITourType>(
+  {
+    name: { type: String, required: true, unique: true },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const tourTypeModel = model<ITourType>("TourType", tourTypeSchema);
+
+const tourSchema = new Schema<ITour>(
+  {
+    title: { type: String, required: true },
+    slug: { type: String, required: true, unique: true },
+    description: { type: String },
+    images: { type: [String], default: [] },
+    location: { type: String },
+    costFrom: { type: Number },
+    startDate: { type: Date },
+    endDate: { type: Date },
+    included: { type: [String], default: [] },
+    excluded: { type: [String], default: [] },
+    amenities: { type: [String], default: [] },
+    tourPlan: { type: [String], default: [] },
+    maxGuest: { type: Number },
+    minAge: { type: Number },
+    division: {
+      type: Schema.Types.ObjectId,
+      ref: "Division",
+      required: [true, "Division id is required"],
+      index: true,
+    },
+    tourType: {
+      type: Schema.Types.ObjectId,
+      ref: "TourType",
+      required: [true, "Tour Type id is required"],
+      index: true,
+    },
+  },
+  { timestamps: true, versionKey: false },
+);
+
+export const tourModel = model<ITour>("Tour", tourSchema);
