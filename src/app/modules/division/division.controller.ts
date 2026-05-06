d6168ec0d53d213630c830531fe7b2432ac9a148
Foysal Ahmmed Limon <afaysal410@gmail.com>
2026-05-06 19:57:05 +0600

    feat: add createDivision controller to handle division creation

---
+++ src/app/modules/division/division.controller.ts
@@ -0,0 +1,19 @@
+import status from "http-status";
+import catchAsync from "../../utils/catchAsync.js";
+import sendResponse from "../../utils/sendResponse.js";
+import { DivisionService } from "./division.service.js";
+
+const createDivision = catchAsync(async (req, res) => {
+  const result = await DivisionService.createDivisionIntoDB(req.body);
+
+  sendResponse(res, {
+    statusCode: status.CREATED,
+    success: true,
+    message: "Division created successfully",
+    data: result,
+  });
+});
+
+export const DivisionControllers = {
+  createDivision,
+};
