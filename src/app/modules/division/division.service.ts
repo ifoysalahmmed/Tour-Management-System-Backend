7c7f121c73bedcd4dd712a5925c1485284d0438b
Foysal Ahmmed Limon <afaysal410@gmail.com>
2026-05-07 17:54:13 +0600

    fix: correct import and variable name for TourModel in division service

--- src/app/modules/division/division.service.ts
+++ src/app/modules/division/division.service.ts
@@ -1,7 +1,7 @@
 import status from "http-status";
 
 import { AppError } from "../../errors/app.error.js";
-import { tourModel } from "../tour/tour.model.js";
+import { TourModel } from "../tour/tour.model.js";
 import type { IDivision } from "./division.interface.js";
 import { DivisionModel } from "./division.model.js";
 
@@ -56,7 +56,7 @@ const deleteDivisionFromDB = async (id: string) => {
     throw new AppError(status.NOT_FOUND, "Division not found");
   }
 
-  const isUsedInTour = await tourModel.exists({ division: id });
+  const isUsedInTour = await TourModel.exists({ division: id });
 
   if (isUsedInTour) {
     throw new AppError(status.CONFLICT, "Division is used in a tour");
