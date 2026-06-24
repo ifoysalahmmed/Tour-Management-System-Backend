d396b2166a968d63b70b24594ca8916beb87ca79
Foysal Ahmmed Limon <afaysal410@gmail.com>
2026-06-24 18:57:04 +0600

    feat: update Redis client export and reorganize route imports

--- src/app/config/redis.config.ts
+++ src/app/config/redis.config.ts
@@ -2,7 +2,7 @@ import { createClient } from "redis";
 
 import { envVars } from "./env.js";
 
-const redisClient = createClient({
+export const redisClient = createClient({
   username: envVars.REDIS.USERNAME,
   password: envVars.REDIS.PASSWORD,
   socket: {
