58ca48dc7d2c539d45f67daee93d54cd7f8077b0
Foysal Ahmmed Limon <afaysal410@gmail.com>
2026-07-04 17:38:54 +0600

    feat: add validation route for payment processing

--- src/app/modules/payment/payment.route.ts
+++ src/app/modules/payment/payment.route.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 
 import checkAuth from "../../middlewares/checkAuth.middleware.js";
+import validateBody from "../../middlewares/validateBody.middleware.js";
 import { UserRole } from "../user/user.interface.js";
 import { PaymentControllers } from "./payment.controller.js";
+import { validateIPNZodSchema } from "./payment.validation.js";
 
 const router = Router();
 
@@ -22,6 +24,12 @@ router.get(
   "/invoice/:paymentId",
   checkAuth(...Object.values(UserRole)),
   PaymentControllers.getInvoiceDownloadUrl,
+);
+
+router.post(
+  "/validate-payment",
+  validateBody(validateIPNZodSchema),
+  PaymentControllers.validatePayment,
 );
 
 export const PaymentRoutes = router;
