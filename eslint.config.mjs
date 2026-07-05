dfaef30beddfd057a64b70e62fa33ed0c294ae7a
Foysal Ahmmed Limon <afaysal410@gmail.com>
2026-07-05 14:02:06 +0600

    feat: update ESLint configuration to enhance rules for unused variables and console warnings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,12 +5,22 @@ import { defineConfig } from "eslint/config";
 import tseslint from "typescript-eslint";
 
 export default defineConfig(
+  {
+    ignores: ["dist/**"],
+  },
   eslint.configs.recommended,
   tseslint.configs.strict,
   tseslint.configs.stylistic,
   {
     rules: {
       "no-console": "warn",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
     },
   },
 );
